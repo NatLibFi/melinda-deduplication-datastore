997804ebf5f997f1717faa33eeb28bc757f3655d
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-09-29 11:06:11 +0300

    Adding not null column fails without default value when altering table with existing data

--- src/schema/migrations/migration-5-to-6.sql
+++ src/schema/migrations/migration-5-to-6.sql
@@ -1,4 +1,4 @@
-alter table record add recordTimestamp DATETIME NOT NULL;
+alter table record add recordTimestamp DATETIME NOT NULL DEFAULT '0000-01-01 00:00:00';
 
 set @record_timestamp_index_exists = (SELECT COUNT(1) IndexIsThere FROM INFORMATION_SCHEMA.STATISTICS WHERE table_schema=DATABASE() AND table_name='record' AND index_name='record_timestamp');
 set @sqlStatement := if( @record_timestamp_index_exists > 0, 'select ''INFO: Index already exists.''', 'create index record_timestamp ON record (recordTimestamp);');
